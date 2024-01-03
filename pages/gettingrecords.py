926a06424c6c4001fbf2a51cefe07c137d821b64
SergioSKA27 <sergioska81@hotmail.com>
2024-01-03 04:52:27 -0600

    Update gettingrecords.py and add .gitignore and .trunk files

--- pages/gettingrecords.py
+++ pages/gettingrecords.py
@@ -1,17 +1,22 @@
 import streamlit as st
-from src.st_xata_connection import XataConnection
+from st_xatadb_connection import XataConnection
 
-xata = st.connection('xata', type=XataConnection)
+xata = st.connection(
+    "xata", type=XataConnection
+)
 
-st.title('Getting records')
+st.title("Getting records")
 
-st.markdown('''
+st.markdown(
+    """
 ---
 In this tutorial, you'll learn how to use st_xatadb_connection to connect to your Xata.io database and query your data.
 
-the general form of a query is:''')
+the general form of a query is:"""
+)
 
-st.code("""
+st.code(
+    """
 from st_xata_connection import XataConnection
 
 xata = st.connection('xata', type=XataConnection)
@@ -28,17 +33,24 @@ data = xata.query("{table_name}", {
     ...
   }
 })
-""",language='python')
+""",
+    language="python",
+)
 
 
-st.markdown('All the requests are optional, so the simplest query request looks like this:')
-st.code("""
+st.markdown(
+    "All the requests are optional, so the simplest query request looks like this:"
+)
+st.code(
+    """
 results = xata.query("Table_Name")
-""")
+"""
+)
 
-st.markdown('The response looks like this:')
+st.markdown("The response looks like this:")
 
-st.code("""
+st.code(
+    """
 {
     "records": [
         {
@@ -60,22 +72,29 @@ st.code("""
             "more": false
         }
     }
-}""",language='json')
+}""",
+    language="json",
+)
 
 
-st.subheader('Getting a single record by ID')
+st.subheader("Getting a single record by ID")
 
-st.markdown('''
+st.markdown(
+    """
 You can retrieve a record with a given ID using a request like this:
-''')
+"""
+)
 
-st.code("""
+st.code(
+    """
 record = xata.get("Table_Name", "my_id")
-""",language='python')
+""",
+    language="python",
+)
 
 
-
-st.markdown('''
+st.markdown(
+    """
 ## Columns Selection
 
 By default, the Query API returns all columns of the queried table. For link columns, only the ID column of the linked
@@ -84,31 +103,41 @@ include columns from linked tables. It's worth noting that the special columns i
 xata.updatedAt are always returned, even if they are not explicitly requested.
 
 For example, if you are only interested in the name and the city of the user, you can make a request like this:
-''')
+"""
+)
 
-st.code("""
+st.code(
+    """
 users = xata.query("Users", {
   "columns": ["name", "city"]
 })
-""",language='python')
+""",
+    language="python",
+)
 
 
-st.markdown('''
+st.markdown(
+    """
 
 ## Selecting Columns from the Linked Tables
 
 The same syntax can be used to select columns from a linked table, therefore adding new columns to the response.
 For example, if you have a table called Users, and a table called Orders, and you want to retrieve the name of the user
 for each order, you can make a request like this:
-''')
+"""
+)
 
-st.code("""
+st.code(
+    """
 results = xata.query("Orders", {"columns": ["id", "user.name"]})
-""",language='python')
+""",
+    language="python",
+)
 
-st.markdown('You can do this transitively as well, for example:')
+st.markdown("You can do this transitively as well, for example:")
 
-st.code("""
+st.code(
+    """
 posts = xata.query("Posts", {
   "columns": [
     "title",
@@ -116,4 +145,6 @@ posts = xata.query("Posts", {
     "author.team.*"
   ]
 })
-""",language='python')
+""",
+    language="python",
+)
