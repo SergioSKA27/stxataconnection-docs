ce9879597edc8340e2c48e4a78e9c390b677edbd
SergioSKA27 <sergioska81@hotmail.com>
2024-01-05 04:32:29 -0600

    Add table management functionality

---
+++ pages/table_management.py
@@ -0,0 +1,71 @@
+import streamlit as st
+from st_xatadb_connection import XataConnection
+
+xata =  st.connection('xata',type=XataConnection)
+
+st.title('Table Management')
+st.divider()
+st.markdown('''
+With Xata, you can create, delete, and rename tables. You can also add and remove columns from tables.
+''')
+
+st.header('Create Table')
+st.markdown('''
+To create a table, you need to specify the table name and the columns and their data types.
+You can do this by using the `create_table` method.
+For example, to create a table named `movie` with two columns, `title` and `description`, you can do the following:
+''')
+
+st.code('''
+import streamlit as st
+from st_xatadb_connection import XataConnection
+
+xata =  st.connection('xata',type=XataConnection)
+
+table_schema = {
+  "columns": [
+      {
+          "name": "title",
+          "type": "string",
+      },
+      {
+          "name": "description",
+          "type": "text",
+          "notNull": True,
+          "defaultValue": "Default text"
+      }
+  ]
+}
+result = xata.create_table('movie', table_schema)
+''')
+
+st.subheader('Delete Table')
+st.markdown('''
+To delete a table, you can use the `delete_table` method.
+''')
+
+st.code('''
+result = xata.delete_table('users')
+''')
+
+st.subheader('Add Columns')
+st.markdown('''
+To add a column to a table, you can use the `create_column` method.
+''')
+
+st.code('''
+result = xata.create_column('users', {'email': 'string'})
+''')
+
+st.subheader('Remove Columns')
+st.markdown('''
+To remove a column from a table, you can use the `delete_column` method.
+''')
+
+st.code('''
+result = xata.delete_column('users', 'email')
+''')
+
+st.markdown('''
+For more information, see the [Xata API reference](https://xata.io/docs/concepts/schema).
+''')
