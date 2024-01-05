2c2e40162dc5d53f4a8d94649e9f55f3234e58d5
SergioSKA27 <sergioska81@hotmail.com>
2024-01-04 20:00:04 -0600

    Add updatingrecs.py with code for updating and replacing records

---
+++ pages/updatingrecs.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from st_xatadb_connection import XataConnection
+
+xata = st.connection('xata', type=XataConnection)
+
+st.title("Updating records")
+
+st.markdown('''
+In order to do a partial update to a record, you can use a request like this:
+''')
+
+st.code("""
+from st_xatadb_connection import XataConnection
+import streamlit as st
+
+xata = st.connection('xata', type=XataConnection)
+
+response = xata.update("table_name", "rec_id", {'column_name': 'value'})
+""")
+
+st.subheader("Replacing a record")
+st.markdown('''
+You can overwrite a full record with a request like this:
+''')
+
+st.code("""
+response = xata.upsert("table_name", "rec_id", {'column_name': 'value'})
+""")
+
+st.markdown('''
+For more information about the update and upsert endpoints, see the [API reference](https://xata.io/docs/sdk/update).
+''')
